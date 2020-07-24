64d3bffc2762b49746fd46559c8abceb03166e20
deepak <dk721572@gmail.com>
2020-07-24 20:09:47 +0530

    ✨ fix-styling & few changes

--- src/logic/main.js
+++ src/logic/main.js
@@ -1,3 +1,22 @@
-//* ᓚᘏᗢ
+import game, { handleUserAction } from './gameState'
+import { TICK_RATE } from './constants'
+import initButtons from './buttons'
 
-console.log('Entry Point')
+export default async function init() {
+  console.log('starting game')
+  initButtons(handleUserAction)
+  let nextTimeToTick = Date.now()
+
+  function nextAnimationFrame() {
+    const now = Date.now()
+
+    if (nextTimeToTick <= now) {
+      game.tick()
+      nextTimeToTick = now + TICK_RATE
+    }
+
+    requestAnimationFrame(nextAnimationFrame)
+  }
+
+  requestAnimationFrame(nextAnimationFrame)
+}
